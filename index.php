83b602bc5db5c90fedbc7c9059f1cdcc3a41ab8b
brendankuchenbecker <brendankuchenbecker@gmail.com>
2017-05-21 18:40:00 -0400

    Remove icon style links from index.php

--- index.php
+++ index.php
@@ -15,9 +15,6 @@ else
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>Increase Efficiency</title>
     <meta name="viewport" content="width=device-width">
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lobster+Two" type="text/css">
-    <link rel="icon" href="https://awsmedia.s3.amazonaws.com/favicon.ico" type="image/ico" >
-    <link rel="shortcut icon" href="https://awsmedia.s3.amazonaws.com/favicon.ico" type="image/ico" >
     <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
     <link rel="stylesheet" href="/styles.css" type="text/css">
 </head>
